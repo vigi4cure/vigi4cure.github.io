6546392ebfd08b351a66b4ea6e5b467019a69e24
vigi4cure <ywen@vigilantglobal.com>
2018-05-02 23:55:04 -0400

    Wed May  2 23:55:04 EDT 2018

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+/usr/bin/python3 segment_plots.py
+/usr/bin/python3 strava_leaderboard.py
+/usr/bin/python3 strava_segments.py
+/usr/bin/git add -A
+/usr/bin/git commit -a -m "$(date)"
+/usr/bin/git push
+
